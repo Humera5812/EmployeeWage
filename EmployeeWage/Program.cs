c07a9aac09feeb6e14069bb562fa2df730538092
Humera5812 <humera198.hshs@gmail.com>
2022-07-20 16:30:23 +0530

    Second Commit

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Wage.GetEmployeeWage();
+        }
+    }
+}
